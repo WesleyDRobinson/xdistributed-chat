c08d01052a076b747ee6096010b40b4f20ad890d
wesley <robinson.wesleyd@gmail.com>
2018-01-29 15:19:59 -0800

    more beautiful

--- src/components/app-shell.js
+++ src/components/app-shell.js
@@ -32,7 +32,7 @@ class AppShell extends HyperHTMLElement {
             console.log(`IPFS node ready with address ${data.id}`)
         }))
 
-        this.className = 'db vh-100 bg-near-white'
+        this.className = 'dt vh-100 w-100 bg-near-white gradientSquare'
         this.render()
     }
 
